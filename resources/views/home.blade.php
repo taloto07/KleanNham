a5d4676a479ebee53be3e24a39e71bff1428c4fe
taloto07 <taloto07@gmail.com>
2017-03-21 22:23:07 +0700

    changed head to feature places

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,8 @@
     <!-- Page Heading -->
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Page Heading
-                <small>Secondary Text</small>
+            <h1 class="page-header">Feature Places
+                <small>Have Fun</small>
             </h1>
         </div>
     </div>
