90fc56eb8eada5ad53adbc329b98acbbbca5bd4b
taloto07 <taloto07@gmail.com>
2017-07-22 21:26:38 +0700

    fix 404 page: take check for auth

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,12 +8,6 @@
         <h1>404</h1>
         <p>Page not found. Please use search in header.</p>
         <a href="{{ route('home') }}" class="btn btn-primary"><i class="fa fa-long-arrow-left"></i> Return Home</a>
-
-        @if (Auth::guest())
-        	<div>you are guest</div>
-        @else
-        	<div>{{ Auth::user()->name }}</div>
-        @endif
       </div>
       <!-- /.warning -->
     </div>
